431d4e1c55fad613bf7c830c307fe9869a0eb974
Ryan Moore <paniclater@gmail.com>
2022-11-13 14:55:20 -0700

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_06_183349) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_13_215235) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lev_admirers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.boolean "is_coming"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
