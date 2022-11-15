9d30b94c2331d820b470eccabfb75e64db243846
Ryan Moore <paniclater@gmail.com>
2022-11-14 20:59:52 -0700

    basic editing of a lev admirer

--- app/controllers/lev_admirers_controller.rb
+++ app/controllers/lev_admirers_controller.rb
@@ -11,15 +11,34 @@ class LevAdmirersController < ApplicationController
     @lev_admirer = LevAdmirer.new(lev_admirer_params)
 
     if @lev_admirer.save
-      redirect_to action: "index"
+      redirect_to action: :index
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @lev_admirer = LevAdmirer.find(params[:id])
+  end
+
+  def update
+    @lev_admirer = LevAdmirer.find(params[:id])
+
+    if @lev_admirer.update(lev_admirer_params)
+      redirect_to action: :edit
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   private
 
   def lev_admirer_params
-    params.require(:lev_admirer).permit(:first_name, :last_name, :phone_number)
+    params.require(:lev_admirer).permit(
+      :first_name,
+      :last_name,
+      :phone_number,
+      :is_coming
+    )
   end
 end
